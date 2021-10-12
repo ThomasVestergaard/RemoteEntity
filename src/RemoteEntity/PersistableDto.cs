911713b40171069a92ab4b4f79198a29053049a5
Thomas Fredsted <mail@thomasvestergaard.com>
2021-10-12 15:04:51 +0200

    Started work on saving PublishTime on entities

---
+++ src/RemoteEntity/PersistableDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace RemoteEntity
+{
+    public class PersistableDto<T>
+    {
+        public DateTimeOffset PublishTime { get; set; }
+        public T Value { get; set; }
+    }
+}
